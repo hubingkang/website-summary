b8f37edbfd20be10231ff068ec58726f1b2efab6
hubingkang <squarewheel@163.com>
2024-12-08 21:01:33 +0800

    feat: init 🎉

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
+# website-summary
+
+`Couldflare Worker AI` combined with examples from `jina.ai`
+
+## test
+
 ```
 npm install
+```
+
+Set the variable [JINA_KEY](https://jina.ai/) in the wrangler.toml file.
+
+```
 npm run dev
 ```
+
+request `http://localhost:8787/summary/https://baidu.com/`
+
+## deploy to cloudflare
 
 ```
 npm run deploy
 ```
+
+- [jina.ai](https://jina.ai/)
+- [Cloudflare Worker AI](https://developers.cloudflare.com/workers-ai/)
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from 'hono'
+import { env } from 'hono/adapter'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/summary/*', async (c) => {
+  const { JINA_KEY } = env<{ JINA_KEY: string }>(c)
+
+  const url = c.req.path.substring('/summary/'.length)
+  const res = await fetch(`https://r.jina.ai/${url}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${JINA_KEY}`,
+    },
+  })
+
+  const str = await res.text()
+
+  const results = await c.env.AI.run('@cf/meta/llama-3-8b-instruct', {
+    messages: [
+      {
+        role: 'user',
+        content: `Summarize the content below：\n ${str}`,
+      },
+    ],
+  })
+
+  return c.json(results)
 })
 
 export default app
